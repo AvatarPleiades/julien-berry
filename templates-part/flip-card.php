a06b55224026f7f0ba1cdd8f213dc8fa13866b2d
AvatarPleiades <berry.julien57@gmail.com>
2023-10-02 08:53:59 +0200

    Retrait tooltip

--- templates-part/flip-card.php
+++ templates-part/flip-card.php
@@ -10,24 +10,12 @@
                         <h1 class="white">Contact</h1>
                         <p class="white">Mes différents réseaux</p>
                         <div class="icon-group">
-                            <div class="tooltip">
-                                <span class="tooltiptext">LinkedIn de Julien Berry</span>
-                            <i class="fab fa-linkedin-in fa-icon"></i>
-                            </div>
-                            <div class="tooltip">
-                                <span class="tooltiptext">GitHub de Julien Berry</span>
-                            <a href="https://github.com/AvatarPleiades" alt="GitHub de Julien Berry" target="_blank"><i class="fab fa-github fa-icon"></i></a>
-                            </div>
+                            <a href="#" aria-label="LinkedIn de Julien Berry" title="LinkedIn de Julien Berry" alt="LinkedIn de Julien Berry"><i class="fab fa-linkedin-in fa-icon"></i></a>
+                            <a href="https://github.com/AvatarPleiades" aria-label="GitHub de Julien Berry" title="GitHub de Julien Berry" alt="GitHub de Julien Berry" target="_blank"><i class="fab fa-github fa-icon"></i></a>
                         </div>
                         <div class="icon-group">
-                            <div class="tooltip">
-                                <span class="tooltiptext">X de Julien Berry</span>
-                            <i class="fab fa-twitter fa-icon"></i>
-                            </div>
-                            <div class="tooltip">
-                                <span class="tooltiptext">Mail de Julien Berry</span>
-                            <a href="mailto:berry.julien57@gmail.com" title="Envoyez-moi un e-mail"><i class="far fa-envelope fa-icon"></i></a>
-                            </div>
+                            <a href="#" aria-label="X de Julien Berry" title="X de Julien Berry" alt="X de Julien Berry"><i class="fab fa-twitter fa-icon"></i></a>
+                            <a href="mailto:berry.julien57@gmail.com" aria-label="Envoyez-moi un e-mail" title="Envoyez-moi un e-mail" alt="Envoyez-moi un e-mail"><i class="far fa-envelope fa-icon"></i></a>
                         </div>
                         <div>
                             <button class="contact-me">
